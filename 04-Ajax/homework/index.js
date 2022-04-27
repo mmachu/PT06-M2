ea8774fe14a4c508999467c23b4041915e423d7c
unknown <matiasmachulini@gmail.com>
2022-04-26 22:25:47 -0300

    improves homework resolution

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,11 +1,30 @@
 const searchAll = () => {
-  $("#lista").children("li").remove();
+  //   $("#lista").children("li").remove();
   $.get("http://localhost:5000/amigos", (data) => {
-    for (user of data) {
-      let li = document.createElement("li");
-      li.innerHTML = user.name;
-      li.id = `li${user.id}`;
-      $("#lista").append(li);
+    let liList = document.querySelectorAll(".friends");
+    let liID = [];
+    liList.forEach((friend) => {
+      liID.push(friend.id);
+    });
+    if (liID.length === 0) {
+      for (user of data) {
+        let li = document.createElement("li");
+        li.innerHTML = user.name;
+        li.id = `li${user.id}`;
+        li.classList.add("friends");
+        $("#lista").append(li);
+      }
+    } else {
+      let dataID = data.map((user) => {
+        return `li${user.id}`;
+      });
+      console.log(dataID);
+      liID.forEach((id, index) => {
+        console.log(`${liID[index]}`);
+        if (!dataID.includes(liID[index])) {
+          $(`#${liID[index]}`).remove();
+        }
+      });
     }
   });
 };
@@ -19,12 +38,14 @@ const searchOne = () => {
 };
 
 const deleteFriend = () => {
+  console.log("delete");
   let value = $("#inputDelete").val();
   $.ajax({
     url: `http://localhost:5000/amigos/${value}`,
     type: "DELETE",
     success: (result) => {
-      $(`#li${value}`).remove();
+      let liText = $(`#li${value}`).html();
+      $(`#li${value}`).html(liText + " (borrado recientemente)");
       let inputDeleteValue = $("#amigo").html();
       inputDeleteValue !== ""
         ? $("#amigo").html(inputDeleteValue + " (borrado recientemente)")
